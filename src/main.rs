3f13b5a1d4d3ea4a7fe2f9fe4bba64f8bf47699e
Hasnain <hasnain@Sunhub-HassanFareed.local>
2026-08-20 01:11:03 +0500

    MVP, Saves notes to a txt

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ struct Cli {
     value: String,
 }
 
+
 fn main(){
     // define argument and value
     let argument = std::env::args().nth(1).expect("Error");
     let value = std::env::args().nth(2).expect("Error");
 
+    // Read the string and convert it into a PathBuf using PathBuf::from
 
     // Create an instance of the cli
     let arg = Cli {argument, value};
@@ -31,7 +33,6 @@ fn main(){
         let log_entry = format!("{}\n", arg.value);
 
         // Write the note in notes.txt
-
         file.write_all(log_entry.as_bytes()).expect("Failed to save note");
         println!("Saved!")
     };
